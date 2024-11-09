a40ed97592e14902e06c49e52a43d8fa5f79e2a1
romeyiow <jimperialyosh@gmail.com>
2024-11-09 12:37:51 +0800

    Fixed useState set to null problem

--- src/UserFetcher.jsx
+++ src/UserFetcher.jsx
@@ -2,19 +2,34 @@ import { useEffect, useState } from "react"
 const UserFetcher = () => {
     const [user, setUser] = useState(null)
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users/3')
-            .then(response => {
-                return response.json()
-            })
-            .then(data => setUser(data))
-            .catch(error => console.log(error))
-    }, [])
-
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/users/3')
+    //         .then(response => {
+    //             return response.json()
+    //         })
+    //         .then(data => setUser(data))
+    //         .catch(error => console.log(error))
+    // }, [])
+     useEffect (() => {
+        fetch('')
+        .then(fetchedData => {return fetchedData.json()})
+        .then(convertedData => {setUser(convertedData)})
+        .catch(error => {console.log(error)})
+     },[])
     return (
         <>
             <h1>User Fetcher Component</h1>
-            {(() => {
+            
+            {user ? (
+                <>
+                    <p>name: {user.name}</p>
+                    <p>email: {user.email}</p>
+                </>) : (
+                <p>Loading...</p>  // Loading message until data is fetched
+            )}
+            {null ? (<>is no longer null</>):(user)}
+
+            {/* {(() => {
                 
                 if (user) {
                     return (
@@ -26,14 +41,7 @@ const UserFetcher = () => {
                 } else {
                    return <p>Loading...</p>
                 }
-            })()}
-            {/* {user ? (
-                <>
-                    <p>name: {user.name}</p>
-                    <p>email: {user.email}</p>
-                </>) : (
-                <p>Loading...</p>  // Loading message until data is fetched
-            )} */}
+            })()} */}
         </>
     )
 }
